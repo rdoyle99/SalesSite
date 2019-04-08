6df2d6978e504d47e03d418ab7de63a2e695505f
Ryan Doyle <rpdoyle1@gmail.com>
2019-04-08 19:43:18 -0400

    Big design push and roadmap

--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -1,12 +1,13 @@
 import React from "react"
+import "./styles/Roadmap.css"
 
 class Roadmap extends React.Component {
   render() {
-    return (<div>
-      <h3>
+    return (<div class="todos">
+      <h3 class="toppings" id="top">
         This is the roadmap of features being built into the Tech Sales Work platform. If you have ideas or feedback, please DM me by heading to my Twitter on the top left!
       </h3>
-      <ul class="list-group list-group-flush">
+      <ul class="listings">
         <li class="list-group-item">Add ability to post jobs</li>
         <li class="list-group-item">Add ability to sort by role and perform search</li>
         <li class="list-group-item">Augment postings with salary and satisfaction data</li>
