f5c01774e214a49ff2405e69482c235499525618
Ryan Doyle <rpdoyle1@gmail.com>
2019-05-25 11:22:09 -0400

    adding sort before new changes

--- src/components/ListingsPage.js
+++ src/components/ListingsPage.js
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
-import React from 'react';
-// import Today from "./listingElements/Today"
-import TodayDisplay from './listingElements/TodayDisplay';
-import Earlier from './listingElements/Earlier';
-import EarlierDisplay from './listingElements/EarlierDisplay';
-
-class ListingsPage extends React.Component {
-  render() {
-    return (
-      <div>
-        {/* <Today /> */}
-=======
 import React from 'react'
-import Today from "./listingElements/Today"
 import TodayDisplay from "./listingElements/TodayDisplay"
 import Earlier from "./listingElements/Earlier"
 import EarlierDisplay from "./listingElements/EarlierDisplay"
@@ -22,8 +8,6 @@ class ListingsPage extends React.Component {
   render() {
     return (
       <div >
-        <Today />
->>>>>>> 6df2d6978e504d47e03d418ab7de63a2e695505f
         <TodayDisplay />
         <Earlier />
         <EarlierDisplay />
---
+++ src/components/YesterdayDisplay.js
@@ -0,0 +1,82 @@
+import React from "react"
+import './ListingDisplay.css'
+import LoadingSpinner from './LoadingSpinner'
+
+//1.20.2019. this is a function that pulls in props of data to the App from jobsData
+//it presents them in a table. I'd like to add a header for when dates change
+
+
+class YesterdayDisplay extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: false,
+      data: [],
+    }
+  }
+
+  componentDidMount() {
+    this.setState({loading: true})
+    fetch("https://sheetsu.com/apis/v1.0su/c3d38a3d5efd/sheets/yesterday")
+      .then( (response) => {
+        return response.json()
+      }).then( (json) => {
+        this.setState({
+            loading: false,
+            data: json,
+          });
+      });
+  }
+
+
+  renderData() {
+    return (
+        <ul>
+          {
+            this.state.data.map(function(row){
+              return <div id="tablePreview" class="table table-hover">
+
+                  <li key={row.id} class="row border-bottom d-inline-flex">
+
+                  <div class="col d-inline-flex columns is-mobile">
+                    <img class="logo" src= {row.logo}/>
+                  </div>
+
+                  <div class="col d-inline-flex columns d-inline-flex txtone">
+                    <h2><strong>{row.company}</strong></h2>
+                  </div>
+
+                  <div class="col d-inline-flex columns is-mobile txttwo">
+                    <h3>{row.title}</h3>
+                  </div>
+
+                  <div class="col d-inline-flex text-muted columns is-mobile txttwo">
+                    <p>{row.location}</p>
+                  </div>
+
+                  <div class="col d-inline-flex columns is-mobile">
+                    <a href={row.url} target="_blank"><button type="button" class="btn btn-success btn-small" >Apply</button></a>
+                  </div>
+
+                </li>
+                </div>
+            })
+          }
+        </ul>
+    );
+  }
+
+
+  render() {
+    const {data, loading} = this.state;
+
+    return (
+      <div class="container-fluid">
+        {loading ? <LoadingSpinner /> : this.renderData()}
+      </div>
+    )
+  }
+}
+
+
+export default YesterdayDisplay;
